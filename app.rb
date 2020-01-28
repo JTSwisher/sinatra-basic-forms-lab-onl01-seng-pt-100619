7f269dbed9bd0f3f50cb1e52033702bb012a324d
Jeff Swisher <jefftswisher@gmail.com>
2020-01-28 03:29:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,11 +11,9 @@ class App < Sinatra::Base
    end 
   
   post '/puppy' do 
-    @puppy = Puppy.new
     @puppy.name = params[:name]
     @puppy.breed = params[:breed]
     @puppy.age = params[:age]
-    @puppy.save
     erb :display_puppy
   end 
   
